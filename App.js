a3f2983e64e3da0bc629aaa6e7ce4c2107fefc57
srinu <vadithesrinunaik09@gmail.com>
2023-02-01 18:41:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+const App = () => {
+
+  return (
+    <center>
+      <div className='loading'>
+      
+      <h1>srinu </h1>
+      <button>CLICK</button>
+    </div>
+    </center>
+  )
+}
+export default App
+
+
+
